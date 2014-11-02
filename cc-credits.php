c20f81e13ffcf716d3d29862e5a5da40372639d0
pixolin <pixolin@gmx.com>
2014-11-02 16:23:58 +0100

    vor Übersetzung

---
+++ cc-credits.php
@@ -0,0 +1,163 @@
+<?php
+/*
+Plugin Name: CC Credits
+Description: Adds a meta box for credits for Creative Commons content.
+Author: Bego Mario Garde
+Author URI: http://garde-medienberatung.de
+Version: 1.0
+License: GPL2
+Text Domain: cc-credits
+Domain Path: /languages/
+*/
+
+/*
+
+    Copyright (C) 2014  Bego Mario Garde <pixolin@gmx.com>
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+// No direct execution of plugin
+defined('ABSPATH') or die();
+
+
+// require file with meta box fields
+require_once('cc-fields.php');
+
+// Add the Meta Box
+function ccc_add_metabox() {
+
+    // define Post Types that will show Meta Box
+    $post_types = array( 'post', 'page' ); //could be extended for Custom Post Types
+
+    foreach( $post_types as $post_type ) {
+        add_meta_box(
+            'ccc_meta_box', // $id
+            'Creative Commons Credits', // $title
+            'ccc_meta_box', // $callback
+            $post_type, // $page
+            'normal', // $context
+            'high' // $priority
+        );
+    }
+}
+
+add_action('add_meta_boxes', 'ccc_add_metabox');
+
+// Callback function for meta box – shows the form in Back End
+function ccc_meta_box() {
+
+    global $custom_meta_fields, $post;
+
+    // Use nonce for verification
+    //echo '<input type="hidden" name="custom_meta_box_nonce" value="'.wp_create_nonce(basename(__FILE__)).'" />';
+    wp_nonce_field( basename( __FILE__ ), 'custom_meta_box_nonce' );
+
+    // Begin the field table and loop
+    echo '<table class="form-table">';
+    foreach ($custom_meta_fields as $field) {
+        // get value of this field if it exists for this post
+        $meta = get_post_meta($post->ID, $field['id'], true);
+        // begin a table row with
+        echo '<tr>
+                <th><label for="'.$field['id'].'">'.$field['label'].'</label></th>
+                <td>';
+                switch($field['type']) {
+
+                    // text
+                    case 'text':
+                    echo '<input type="text" name="'.$field['id'].'" id="'.$field['id'].'" value="'.$meta.'" size="30" />';
+                    break;
+
+                    // select
+                    case 'select':
+                    echo '<select name="'.$field['id'].'" id="'.$field['id'].'">';
+                    foreach ($field['options'] as $option) {
+                        echo '<option', $meta == $option['value'] ? ' selected="selected"' : '', ' value="'.$option['value'].'">'.$option['label'].'</option>';
+                    }
+                    break;
+
+                } //end switch
+        echo '</td></tr>';
+    } // end foreach
+    echo '</table>'; // end table
+
+    echo ccc_get_post_custom( $post->ID );
+}
+
+// Save the Data
+function ccc_save_meta($post_id) {
+    global $custom_meta_fields;
+
+    // verify nonce
+    if (!isset($_POST['custom_meta_box_nonce']) ||
+        !wp_verify_nonce($_POST['custom_meta_box_nonce'], basename(__FILE__)))
+        return $post_id;
+    // check autosave
+    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE)
+        return $post_id;
+    // check permissions
+    if ('page' == $_POST['post_type']) {
+        if (!current_user_can('edit_page', $post_id))
+            return $post_id;
+        } elseif (!current_user_can('edit_post', $post_id)) {
+            return $post_id;
+    }
+
+    // loop through fields and save the data
+    foreach ($custom_meta_fields as $field) {
+        $old = get_post_meta($post_id, $field['id'], true);
+        $new = $_POST[$field['id']];
+        if ($new && $new != $old) {
+            update_post_meta($post_id, $field['id'], $new);
+        } elseif ('' == $new && $old) {
+            delete_post_meta($post_id, $field['id'], $old);
+        }
+    } // end foreach
+}
+add_action('save_post', 'ccc_save_meta');
+
+/*
+ * If a license was chosen, return a line with name of image, link to source,
+ * copyright date and author, Creative Commons License, link to human readable
+ * summary of license and license version.
+ */
+function ccc_get_post_custom($post_id) {
+
+
+   $ccc = get_post_custom($post_id);
+    if(!empty($ccc['ccc_license'][0])) {
+        $ccc_url     = $ccc['ccc_url'][0];
+        $ccc_image   = $ccc['ccc_image'][0];
+        $ccc_year    = $ccc['ccc_year'][0];
+        $ccc_author  = $ccc['ccc_author'][0];
+        $ccc_license = $ccc['ccc_license'][0];
+        $ccc_version = $ccc['ccc_version'][0];
+
+        return '<hr>'.
+                __('Image', 'cc-credits').': <a href="http://'.$ccc_url.'">'.$ccc_image.'</a>
+                &copy' .$ccc_year.' '.$ccc_author.',
+                <a href="http://creativecommons.org/licenses/'.strtolower($ccc_license).'/'.$ccc_version.'" target="_blank" rel="nofollow">CC '.$ccc_license.'</a>,
+                '.$ccc_version;
+    }
+
+}
+
+// Provide Credit under each Blog Post and Page, only for main query.
+function ccc_credit($content) {
+    if(is_main_query()) {
+      return $content . ccc_get_post_custom($post_id);
+    }
+}
+add_filter('the_content', 'ccc_credit');
---
+++ cc-fields.php
@@ -0,0 +1,94 @@
+<?php
+/*
+ * Set an array of fields for the meta box
+ */
+
+// No direct execution of plugin
+defined('ABSPATH') or die();
+
+$prefix = 'ccc_';
+$custom_meta_fields = array(
+    array(
+        'label'=> __('Title of Image', 'cc-credits'),
+        'id'    => $prefix.'image',
+        'type'  => 'text'
+    ),
+
+    array(
+        'label'=> __('Year of Creation', 'cc-credits'),
+        'id'    => $prefix.'year',
+        'type'  => 'text'
+    ),
+
+    array(
+        'label'=> __('Author', 'cc-credits'),
+        'id'    => $prefix.'author',
+        'type'  => 'text'
+    ),
+
+    array(
+        'label'=> __('URL', 'cc-credits'),
+        'id'    => $prefix.'url',
+        'type'  => 'text'
+    ),
+
+    array(
+        'label'=> __('License', 'cc-credits'),
+        'id'    => $prefix.'license',
+        'type'  => 'select',
+        'options' => array (
+            'None' => array (
+                'label' => 'None/Public Domain',
+                'value' => ''
+            ),
+            'BY' => array (
+                'label' => 'Attribution',
+                'value' => 'BY'
+            ),
+            'BY-SA' => array (
+                'label' => 'Attribution ShareAlike',
+                'value' => 'BY-SA'
+            ),
+            'BY-ND' => array (
+                'label' => 'Attribution NoDerivs',
+                'value' => 'BY-ND'
+            ),
+            'BY-NC' => array (
+                'label' => 'Attribution NonCommercial',
+                'value' => 'BY-NC'
+            ),
+            'BY-NC-SA' => array (
+                'label' => 'Attribution NonCommercial ShareAlike',
+                'value' => 'BY-NC-SA'
+            ),
+            'BY-NC-ND' => array (
+                'label' => 'Attribution NonCommercial NoDerivs',
+                'value' => 'BY-NC-ND'
+            )
+        )
+    ),
+    array(
+        'label'=> __('Version', 'cc-credits'),
+        'id'    => $prefix.'version',
+        'type'  => 'select',
+        'options' => array (
+
+            '2.0' => array (
+                'label' => '2.0',
+                'value' => '2.0'
+            ),
+            '3.0' => array (
+                'label' => '3.0',
+                'value' => '3.0'
+            ),
+            '4.0' => array (
+                'label' => '4.0',
+                'value' => '4.0'
+            ),
+            '1.0' => array (
+                'label' => '1.0',
+                'value' => '1.0'
+            )
+        )
+    )
+);
